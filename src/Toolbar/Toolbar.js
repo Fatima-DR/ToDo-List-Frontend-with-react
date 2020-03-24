c72dad1506b66327439955b4cd990596125c84b6
Fatima-DR <fatima.duratovic@gmail.com>
2020-03-24 22:16:02 +0000

    updated toolbox file to be in line

--- src/Toolbar/Toolbar.js
+++ src/Toolbar/Toolbar.js
@@ -2,26 +2,23 @@ import React from 'react';
 
 function Toolbar(props) {
     return (
-         <div className="container">
-            <div className="row">
-                <div className="col -12">
-                    <div className="form-group " id="textbanner">
-                        <input type="text" className="form-control" placeholder="Create Task" aria-label="Create Task"
-                            aria-describedby="button-addon2" />
-                    </div>
+        <div className="container">
+            <div className="row mb-3">
+                <div className="col -5">
+                    <input type="text" className="form-control" placeholder="Create Task" />
+                </div>
+                <div className="col -1">
+                    <label className="form-check-label" htmlFor="urgentCheck1">Urgent?</label><br />
+                    <input type="checkbox" className="form-check-input" id="urgemtCheck1" />
+                </div>
+                <div className="col -3">
+                    <input type="date" className="form-control" />
+                </div>
+                <div className="col -1">
+                    <button className="btn btn-secondary" type="button" id="button-addon2">Add Task{props.count}</button>
                 </div>
             </div>
-            <div className="row">
-                <div className="col -12">
-                    <div className="form-group form-check">
-                        <label className="form-check-label" htmlFor="urgentCheck1">Urgent?</label><br />
-                        <input type="checkbox" className="form-check-input" id="urgemtCheck1" />
-                        <input type="date" className="form-control" />
-                        <button className="btn btn-secondary" type="button" id="button-addon2">Add Task{props.count}</button>
-                    </div>
-                </div>
-            </div>
-         </div>
+        </div>
 
 
     )
