c09da488f28a93d3b60b89fcce9ca492f500c208
Fatima-DR <fatima.duratovic@gmail.com>
2020-04-26 11:45:53 +0100

    Updates

---
+++ precache-manifest.39ee5a1b3783d7e098f3481460047b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35cc26d64c1c358bf8958cd4914e2111",
+    "url": "/todolist_frontend/index.html"
+  },
+  {
+    "revision": "ee8d4f55e757674cfc32",
+    "url": "/todolist_frontend/static/css/main.ea334df3.chunk.css"
+  },
+  {
+    "revision": "ab81df4c3123f1fa7ef6",
+    "url": "/todolist_frontend/static/js/2.eecd37b4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todolist_frontend/static/js/2.eecd37b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8d4f55e757674cfc32",
+    "url": "/todolist_frontend/static/js/main.009ab7bc.chunk.js"
+  },
+  {
+    "revision": "c891fc81c464a91b38ce",
+    "url": "/todolist_frontend/static/js/runtime-main.06c99f87.js"
+  }
+]);
